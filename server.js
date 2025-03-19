c5a248a684f06204decb4541c09898db3472248f
SATIROCONSULTORIA <satiroelias@gmail.com>
2025-03-19 03:18:19 -0300

    alterado

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Defina a porta corretamente
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Aplicação rodando com sucesso!');
+});
+
+// Certifique-se de que esta linha está presente e correta
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
